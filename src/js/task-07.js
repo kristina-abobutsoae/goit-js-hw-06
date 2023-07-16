770ad4d41c74b3b87bbc4c921447c84898e0dd45
kristina-abobutsoae <128186154+kristina-abobutsoae@users.noreply.github.com>
2023-07-16 22:02:44 +0200

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const fontSizeControl = document.querySelector('input#font-size-control');
-const textElement = document.querySelector('span#text');
-
-fontSizeControl.addEventListener('input', () => {
-  const fontSize = fontSizeControl.value + 'px';
-  textElement.style.fontSize = fontSize;
-});
